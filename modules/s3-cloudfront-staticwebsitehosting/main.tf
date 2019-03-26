05f34f672f8940a51267df887876116ba1318e49
Joshua Walsh <josh@ymindustries.com>
2019-03-26 22:28:22 +0800

    Explicitly set S3 Public Access Block

--- modules/s3-cloudfront-staticwebsitehosting/main.tf
+++ modules/s3-cloudfront-staticwebsitehosting/main.tf
@@ -7,6 +7,15 @@ resource "aws_s3_bucket" "gatsby_static_bucket" {
     }
 }
 
+resource "aws_s3_bucket_public_access_block" "gatsby_static_bucket_publicaccess" {
+    bucket = "${aws_s3_bucket.gatsby_static_bucket.id}"
+
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
+
 data "aws_iam_policy_document" "gatsby_static_bucket_policy_document" {
     statement {
         actions = [
