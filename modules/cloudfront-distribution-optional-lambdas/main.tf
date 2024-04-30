dcff1c0dc56ad9fa6f41879ea634dade0ab6d5fd
Yoshi Walsh <ym@ymindustries.com>
2024-04-30 12:30:58 +1000

    Remove workaround for Terraform <0.12's lack of conditional blocks

---
+++ modules/cloudfront-distribution-optional-lambdas/main.tf
@@ -0,0 +1,104 @@
+resource "aws_cloudfront_distribution" "distribution" {
+    enabled = "${var.enabled}"
+    aliases = "${var.aliases}"
+
+    http_version = "${var.http_version}"
+    is_ipv6_enabled = "${var.is_ipv6_enabled}"
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
+
+    origin {
+        origin_id = "main"
+        domain_name = "${var.origin_domain_name}"
+
+        dynamic s3_origin_config {
+            for_each = var.origin_access_identity != "" ? [true] : []
+
+            origin_access_identity = var.origin_access_identity
+        }
+
+        dynamic custom_origin_config {
+            for_each = var.origin_access_identity == "" ? [true] : []
+
+            http_port = var.custom_origin_http_port
+            https_port = var.custom_origin_https_port
+            origin_protocol_policy = var.custom_origin_protocol_policy
+            origin_ssl_protocls = var.custom_origin_ssl_protocols
+        }
+    }
+
+    viewer_certificate {
+        acm_certificate_arn = "${var.acm_certificate_arn}"
+        iam_certificate_id = "${var.iam_certificate_id}"
+        minimum_protocol_version = "${var.minimum_protocol_version}"
+        ssl_support_method = "${var.ssl_support_method}"
+    }
+
+    default_cache_behavior {
+        target_origin_id = "main"
+        min_ttl = "${var.min_ttl}"
+        default_ttl = "${var.default_ttl}"
+        max_ttl = "${var.max_ttl}"
+        compress = "${var.compress}"
+        viewer_protocol_policy = "${var.viewer_protocol_policy}"
+        allowed_methods = "${var.allowed_methods}"
+        cached_methods = "${var.cached_methods}"
+        forwarded_values {
+            cookies {
+                forward = "${var.forward_cookies}"
+            }
+            query_string = "${var.forward_query}"
+        }
+
+        dynamic lambda_function_association {
+            for_each = var.viewerrequest_lambda_enabled ? [true] : []
+
+            event_type = "viewer-request"
+            lambda_arn = var.viewerrequest_lambda_qualifiedarn
+            include_body = var.viewerrequest_lambda_includebody
+        }
+
+        dynamic lambda_function_association {
+            for_each = var.originrequest_lambda_enabled
+
+            event_type = "origin-request"
+            lambda_arn = var.originrequest_lambda_qualifiedarn
+            include_body = var.originrequest_lambda_includebody
+        }
+
+        dynamic lambda_function_association {
+            for_each = var.originresponse_lambda_enabled
+
+            event_type = "origin-response"
+            lambda_arn = var.originresponse_lambda_qualifiedarn
+            include_body = var.originresponse_lambda_includebody
+        }
+
+        dynamic lambda_function_association {
+            for_each = var.viewerresponse_lambda_enabled ? [true] : []
+
+            event_type = "viewer-response"
+            lambda_arn = var.viewerresponse_lambda_qualifiedarn
+            include_body = var.viewerresponse_lambda_includebody
+        }
+    }
+
+    dynamic custom_error_response {
+        for_each = var.custom_response_403_enabled ? [true] : []
+
+        error_code = 403
+        response_type = var.custom_response_403_code
+        response_page_path = var.custom_response_403_page_path
+    }
+
+    dynamic custom_error_response {
+        for_each = var.custom_response_404_enabled ? [true] : []
+
+        error_code = 404
+        response_type = var.custom_response_404_code
+        response_page_path = var.custom_response_404_page_path
+    }
+}
--- modules/s3-cloudfront-originaccessidentity/main.tf
+++ modules/s3-cloudfront-originaccessidentity/main.tf
@@ -131,56 +131,13 @@ resource "aws_lambda_function" "gatsby_originresponse_lambda" {
     }
 }
 
-module "gatsby_static_distribution" {
-    source = "../cloudfront-distribution-optional-lambdas"
-
-    enabled = true
-    aliases = ["${var.domain}"]
-    http_version = "http2"
-    is_ipv6_enabled = true
-
-    origin_domain_name = "${aws_s3_bucket.gatsby_static_bucket.bucket_regional_domain_name}"
-    origin_access_identity = "${aws_cloudfront_origin_access_identity.gatsby_oai.cloudfront_access_identity_path}"
-
-    acm_certificate_arn = "${var.acm_certificate_arn}"
-    iam_certificate_id = "${var.iam_certificate_id}"
-    minimum_protocol_version = "${var.https_minimum_protocol_version}"
-    ssl_support_method = "${var.https_support_non_sni ? "vip" : "sni-only"}"
-
-    min_ttl = "${var.cache_all_objects ? 31536000 : 0}"
-    default_ttl = "${var.cache_all_objects ? 31536000 : 0}"
-    max_ttl = 31536000
-    compress = true
-    viewer_protocol_policy = "${var.https_redirect ? "redirect-to-https" : "allow-all"}"
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods = ["GET", "HEAD", "OPTIONS"]
-    forward_cookies = "none"
-    forward_query = false
-
-    custom_response_403_enabled = true
-    custom_response_403_code = "404"
-    custom_response_403_page_path = "/${var.error_document}"
-    
-    viewerrequest_lambda_enabled = "${var.cloudfront_lambda_viewerrequest_enabled}"
-    viewerrequest_lambda_qualifiedarn = "${var.cloudfront_lambda_viewerrequest_qualifiedarn}"
-    viewerrequest_lambda_includebody = false
-    originrequest_lambda_enabled = true
-    originrequest_lambda_qualifiedarn = "${aws_lambda_function.gatsby_originrequest_lambda.qualified_arn}"
-    originrequest_lambda_includebody = false
-    originresponse_lambda_enabled = true
-    originresponse_lambda_qualifiedarn = "${aws_lambda_function.gatsby_originresponse_lambda.qualified_arn}"
-    originresponse_lambda_includebody = false
-    viewerresponse_lambda_enabled = "${var.cloudfront_lambda_viewerresponse_enabled}"
-    viewerresponse_lambda_qualifiedarn = "${var.cloudfront_lambda_viewerresponse_qualifiedarn}"
-    viewerresponse_lambda_includebody = false
-}
-
-/*resource "aws_cloudfront_distribution" "gatsby_static_distribution" {
+resource "aws_cloudfront_distribution" "gatsby_static_distribution" {
     enabled = true
     aliases = ["${var.domain}"]
 
     http_version = "http2"
     is_ipv6_enabled = true
+
     restrictions {
         geo_restriction {
             restriction_type = "none"
@@ -218,36 +175,49 @@ module "gatsby_static_distribution" {
             }
             query_string = false
         }
+
+        dynamic lambda_function_association {
+            for_each = var.cloudfront_lambda_viewerrequest_enabled ? [true] : []
+
+            event_type = "viewer-request"
+            lambda_arn = var.cloudfront_lambda_viewerrequest_qualifiedarn
+            include_body = false
+        }
+
         lambda_function_association {
             event_type = "origin-request"
-            lambda_arn = "${aws_lambda_function.gatsby_originrequest_lambda.qualified_arn}"
+            lambda_arn = aws_lambda_function.gatsby_originrequest_lambda.qualified_arn
             include_body = false
         }
+
         lambda_function_association {
             event_type = "origin-response"
-            lambda_arn = "${aws_lambda_function.gatsby_originresponse_lambda.qualified_arn}"
+            lambda_arn = aws_lambda_function.gatsby_originresponse_lambda.qualified_arn
             include_body = false
         }
-        
-        lambda_function_association {
-            event_type = "${var.cloudfront_lambda_viewerrequest == "" ? "" : "viewer-request"}"
-            lambda_arn = "${var.cloudfront_lambda_viewerrequest == "" ? "" : var.cloudfront_lambda_viewerrequest}"
-            include_body = false
-        }
+
         # Can't do this because the origin-request and origin-response Lambda functions are already used. Instead, those functions include a pass-through mechanism.
-        # lambda_function_association {
-        #     event_type = "${var.cloudfront_lambda_originrequest == "" ? "" : "origin-request"}"
-        #     lambda_arn = "${var.cloudfront_lambda_originrequest == "" ? "" : var.cloudfront_lambda_originrequest}"
+        # dynamic lambda_function_association {
+        #     for_each = var.cloudfront_lambda_originrequest_enabled
+
+        #     event_type = "origin-request"
+        #     lambda_arn = var.cloudfront_lambda_originrequest_qualifiedarn
         #     include_body = false
         # }
-        # lambda_function_association {
-        #     event_type = "${var.cloudfront_lambda_originresponse == "" ? "" : "origin-response"}"
-        #     lambda_arn = "${var.cloudfront_lambda_originresponse == "" ? "" : var.cloudfront_lambda_originresponse}"
+
+        # dynamic lambda_function_association {
+        #     for_each = var.cloudfront_lambda_originresponse_enabled
+
+        #     event_type = "origin-response"
+        #     lambda_arn = var.cloudfront_lambda_originresponse_qualifiedarn
         #     include_body = false
         # }
-        lambda_function_association {
-            event_type = "${var.cloudfront_lambda_viewerresponse == "" ? "" : "viewer-response"}"
-            lambda_arn = "${var.cloudfront_lambda_viewerresponse == "" ? "" : var.cloudfront_lambda_viewerresponse}"
+
+        dynamic lambda_function_association {
+            for_each = var.cloudfront_lambda_viewerresponse_enabled ? [true] : []
+
+            event_type = "viewer-response"
+            lambda_arn = var.cloudfront_lambda_viewerresponse_qualifiedarn
             include_body = false
         }
     }
@@ -257,4 +227,4 @@ module "gatsby_static_distribution" {
         response_code = 404
         response_page_path = "/${var.error_document}"
     }
-}*/
+}
