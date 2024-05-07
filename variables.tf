6a75e9816ef3449933205f18a7b44840a1a2b012
Yoshi Walsh <yoshi@yoshiwalsh.me>
2024-05-07 23:22:00 +1000

    Remove unused variable

--- variables.tf
+++ variables.tf
@@ -9,12 +9,6 @@ variable "domain_route53_zones" {
     description = "List of zone names of all R53 zones which we need to create domains in. Zone names must include the trailing '.'"
 }
 
-variable "region" {
-    type = string
-    description = "The AWS region to create resources in."
-    default = "us-east-1"
-}
-
 variable "redirect_sources" {
     type = list
     default = []
